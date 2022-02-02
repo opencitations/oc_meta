16851aa159818200191d9462b92217727779aabc
arcangelo7 <arcangelomas@gmail.com>
2022-02-02 17:58:46 +0100

    coci_process now works with zip files

--- plugins/coci/get_dois_from_coci.py
+++ plugins/coci/get_dois_from_coci.py
@@ -1,10 +1,15 @@
-import csv, os
+import csv
+import io
+import os
 from tqdm import tqdm
+from zipfile import ZipFile
 
 
 def get_dois_from_coci(coci_dir:str, output_file_path:str, verbose:bool=False) -> None:
     dois_found = set()
     if os.path.exists(output_file_path):
+        if verbose:
+            print(f'[INFO: coci_process] Looking for previously stored DOIs')
         with open(output_file_path, 'r', encoding='utf8', newline='') as f:
             reader = csv.DictReader(f)
             for row in reader:
@@ -12,27 +17,55 @@ def get_dois_from_coci(coci_dir:str, output_file_path:str, verbose:bool=False) -
         if verbose:
             print(f'[INFO: coci_process] {len(dois_found)} DOIs found')
     if verbose:
-        file_count = sum(len(files) for _, _, files in os.walk(coci_dir))
-        pbar = tqdm(total=file_count)
+        pbar = tqdm(total=get_files_count(coci_dir))
     for fold, _, files in os.walk(coci_dir):
         for file in files:
-            output_csv = list()
             if file.endswith('.csv'):
+                output_csv = list()
                 data = csv.DictReader(open(os.path.join(fold, file), 'r', encoding='utf8'))
-                for row in data:
-                    citing = row['citing']
-                    cited = row['cited']
-                    if citing not in dois_found:
-                        output_csv.append({'doi':row['citing']})
-                        dois_found.add(citing)
-                    if cited not in dois_found:
-                        output_csv.append({'doi':row['cited']})
-                        dois_found.add(cited)
-                with open(output_file_path, 'a', encoding='utf8', newline='') as f:
-                    writer = csv.DictWriter(f, fieldnames=['doi'])
-                    writer.writeheader()
-                    writer.writerows(output_csv)
-            pbar.update(1)
-    pbar.close()
+                process_data(data, dois_found, output_csv)
+                store_data(output_file_path, output_csv)
+                if verbose:
+                    pbar.update()
+            elif file.endswith('.zip'):
+                with ZipFile(os.path.join(coci_dir, fold, file), 'r') as archive:
+                    for name in archive.namelist():
+                        output_csv = list()
+                        with archive.open(name) as infile:
+                            data = csv.DictReader(io.TextIOWrapper(infile, 'utf-8'))
+                            process_data(data, dois_found, output_csv)
+                            store_data(output_file_path, output_csv)
+                            if verbose:
+                                pbar.update()
     if verbose:
+        pbar.close()
         print(f'[INFO: COCI_PROCESS] {len(dois_found)} DOIs stored')
+
+def process_data(data:csv.DictReader, dois_found:set, output_csv:list) -> None:
+    for row in data:
+        citing = row['citing']
+        cited = row['cited']
+        if citing not in dois_found:
+            output_csv.append({'doi':row['citing']})
+            dois_found.add(citing)
+        if cited not in dois_found:
+            output_csv.append({'doi':row['cited']})
+            dois_found.add(cited)
+
+def store_data(output_file_path:str, output_csv:str):
+    with open(output_file_path, 'a', encoding='utf8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=['doi'])
+        writer.writeheader()
+        writer.writerows(output_csv)
+
+def get_files_count(coci_dir:str) -> int:
+    if any(file.endswith('.csv') for  _, _, files in os.walk(coci_dir) for file in files):
+        file_count = sum(len(files) for _, _, files in os.walk(coci_dir))
+    elif any(file.endswith('.zip') for  _, _, files in os.walk(coci_dir) for file in files):
+        file_count = 0
+        for fold, _, files in os.walk(coci_dir):
+            for file in files:
+                with ZipFile(os.path.join(coci_dir, fold, file), 'r') as archive:
+                    file_count += len(archive.namelist())
+    return file_count
+
