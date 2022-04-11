bed7d65ca1e666558b04a7011658ed20eee6e473
arcangelo7 <arcangelomas@gmail.com>
2022-04-11 20:12:58 +0200

    test_meta_process fixed on Unix

--- tdd/meta_process_test.py
+++ tdd/meta_process_test.py
@@ -31,6 +31,8 @@ class test_ProcessTest(unittest.TestCase):
             {'id': 'doi:10.9799/ksfan.2012.25.1.069 meta:br/0605', 'title': 'Nonthermal Sterilization And Shelf-life Extension Of Seafood Products By Intense Pulsed Light Treatment', 'author': 'Cheigh, Chan-Ick [orcid:0000-0003-2542-5788 meta:ra/0603]; Mun, Ji-Hye [meta:ra/0604]; Chung, Myong-Soo [meta:ra/0605]', 'pub_date': '2012-03-31', 'venue': 'The Korean Journal Of Food And Nutrition [issn:1225-4339 meta:br/0603]', 'volume': '25', 'issue': '1', 'page': '69-76', 'type': 'journal article', 'publisher': 'The Korean Society Of Food And Nutrition [crossref:4768 meta:ra/0606]', 'editor': 'Chung, Myong-Soo [orcid:0000-0002-9666-2513 meta:ra/0607]'}, 
             {'id': 'doi:10.9799/ksfan.2012.25.1.077 meta:br/0606', 'title': 'Properties Of Immature Green Cherry Tomato Pickles', 'author': 'Koh, Jong-Ho [meta:ra/0608]; Shin, Hae-Hun [meta:ra/0609]; Kim, Young-Shik [orcid:0000-0001-5673-6314 meta:ra/06010]; Kook, Moo-Chang [meta:ra/06011]', 'pub_date': '2012-03-31', 'venue': 'The Korean Journal Of Food And Nutrition [issn:1225-4339 meta:br/0603]', 'volume': '', 'issue': '2', 'page': '77-82', 'type': 'journal article', 'publisher': 'The Korean Society Of Food And Nutrition [crossref:4768 meta:ra/0606]', 'editor': ''}]
         shutil.rmtree(os.path.join(BASE_DIR, 'output'))
+        output = sorted(sorted(d.items()) for d in output)
+        expected_output = sorted(sorted(d.items()) for d in expected_output)
         self.assertEqual(output, expected_output)
 
     def test_run_meta_process_two_workers(self):
@@ -48,6 +50,8 @@ class test_ProcessTest(unittest.TestCase):
             {'id': 'issn:1524-4539 issn:0009-7322 meta:br/06102', 'title': 'Circulation', 'author': '', 'pub_date': '', 'venue': '', 'volume': '', 'issue': '', 'page': '', 'type': 'journal', 'publisher': '', 'editor': ''}, 
             {'id': 'doi:10.9799/ksfan.2012.25.1.069 meta:br/06201', 'title': 'Nonthermal Sterilization And Shelf-life Extension Of Seafood Products By Intense Pulsed Light Treatment', 'author': 'Cheigh, Chan-Ick [orcid:0000-0003-2542-5788 meta:ra/06201]; Mun, Ji-Hye [meta:ra/06202]; Chung, Myong-Soo [meta:ra/06203]', 'pub_date': '2012-03-31', 'venue': 'The Korean Journal Of Food And Nutrition [issn:1225-4339 meta:br/06203]', 'volume': '25', 'issue': '1', 'page': '69-76', 'type': 'journal article', 'publisher': 'The Korean Society Of Food And Nutrition [crossref:4768 meta:ra/06204]', 'editor': 'Chung, Myong-Soo [orcid:0000-0002-9666-2513 meta:ra/06205]'}, 
             {'id': 'doi:10.9799/ksfan.2012.25.1.077 meta:br/06202', 'title': 'Properties Of Immature Green Cherry Tomato Pickles', 'author': 'Koh, Jong-Ho [meta:ra/06206]; Shin, Hae-Hun [meta:ra/06207]; Kim, Young-Shik [orcid:0000-0001-5673-6314 meta:ra/06208]; Kook, Moo-Chang [meta:ra/06209]', 'pub_date': '2012-03-31', 'venue': 'The Korean Journal Of Food And Nutrition [issn:1225-4339 meta:br/06203]', 'volume': '', 'issue': '2', 'page': '77-82', 'type': 'journal article', 'publisher': 'The Korean Society Of Food And Nutrition [crossref:4768 meta:ra/06204]', 'editor': ''}]
+        output = sorted(sorted(d.items()) for d in output)
+        expected_output = sorted(sorted(d.items()) for d in expected_output)
         self.assertEqual(output, expected_output)
 
 
