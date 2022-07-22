177a8c893c23b1e0ebfb08dfaa9bff9da2f6cf7c
arcangelo7 <arcangelomas@gmail.com>
2022-07-22 17:01:08 +0200

    /search/{fields}/{text}

--- api/oc_meta_addon.py
+++ api/oc_meta_addon.py
@@ -16,8 +16,31 @@
 
 __author__ = 'arcangelo7'
 
-from oc_ocdm.graph import GraphEntity
-from rdflib import URIRef
+import re
+
+URI_TYPE_DICT = {
+    'http://purl.org/spar/fabio/ArchivalDocument': 'archival document', 
+    'http://purl.org/spar/fabio/Book': 'book', 
+    'http://purl.org/spar/fabio/BookChapter': 'book chapter', 
+    'http://purl.org/spar/doco/Part': 'book part', 
+    'http://purl.org/spar/fabio/ExpressionCollection': 'book section', 
+    'http://purl.org/spar/fabio/BookSeries': 'book series', 
+    'http://purl.org/spar/fabio/BookSet': 'book set', 
+    'http://purl.org/spar/fabio/DataFile': 'dataset', 
+    'http://purl.org/spar/fabio/Thesis': 'dissertation', 
+    'http://purl.org/spar/fabio/Journal': 'journal', 
+    'http://purl.org/spar/fabio/JournalArticle': 'journal article', 
+    'http://purl.org/spar/fabio/JournalIssue': 'journal issue', 
+    'http://purl.org/spar/fabio/JournalVolume': 'journal volume', 
+    'http://purl.org/spar/fr/ReviewVersion': 'peer_review', 
+    'http://purl.org/spar/fabio/AcademicProceedings': 'proceedings', 
+    'http://purl.org/spar/fabio/ProceedingsPaper': 'proceedings article', 
+    'http://purl.org/spar/fabio/ReferenceBook': 'reference book', 
+    'http://purl.org/spar/fabio/ReferenceEntry': 'reference entry', 
+    'http://purl.org/spar/fabio/ReportDocument': 'report', 
+    'http://purl.org/spar/fabio/Series': 'series', 
+    'http://purl.org/spar/fabio/SpecificationDocument': 'standard', 
+    'http://purl.org/spar/fabio/WebContent': 'web content'}
 
 
 def split_ids(literal_values:str) -> str:
@@ -36,51 +59,106 @@ def create_metadata_output(results):
         output_results.append(output_result)
     return output_results, True
 
-def __postprocess_type(ugly_types:str) -> str:
-    ugly_types_list = [URIRef(type_uri) for type_uri in ugly_types.split(' ;and; ')]
-    beautiful_type = ''
-    if GraphEntity.iri_archival_document in ugly_types_list:
-        beautiful_type = 'archival document'
-    elif GraphEntity.iri_book in ugly_types_list:
-        beautiful_type = 'book'
-    elif GraphEntity.iri_book_chapter in ugly_types_list:
-        beautiful_type = 'book chapter'
-    elif GraphEntity.iri_part in ugly_types_list:
-        beautiful_type = 'book part'
-    elif GraphEntity.iri_expression_collection in ugly_types_list:
-        beautiful_type = 'book section'
-    elif GraphEntity.iri_book_series in ugly_types_list:
-        beautiful_type = 'book series'
-    elif GraphEntity.iri_book_set in ugly_types_list:
-        beautiful_type = 'book set'
-    elif GraphEntity.iri_data_file in ugly_types_list:
-        beautiful_type = 'dataset'
-    elif GraphEntity.iri_thesis in ugly_types_list:
-        beautiful_type = 'dissertation'
-    elif GraphEntity.iri_journal in ugly_types_list:
-        beautiful_type = 'journal'
-    elif GraphEntity.iri_journal_article in ugly_types_list:
-        beautiful_type = 'journal article'
-    elif GraphEntity.iri_journal_issue in ugly_types_list:
-        beautiful_type = 'journal issue'
-    elif GraphEntity.iri_journal_volume in ugly_types_list:
-        beautiful_type = 'journal volume'
-    elif GraphEntity.iri_peer_review in ugly_types_list:
-        beautiful_type = 'peer_review'
-    elif GraphEntity.iri_academic_proceedings in ugly_types_list:
-        beautiful_type = 'proceedings'
-    elif GraphEntity.iri_proceedings_paper in ugly_types_list:
-        beautiful_type = 'proceedings article'
-    elif GraphEntity.iri_reference_book in ugly_types_list:
-        beautiful_type = 'reference book'
-    elif GraphEntity.iri_reference_entry in ugly_types_list:
-        beautiful_type = 'reference entry'
-    elif GraphEntity.iri_report_document in ugly_types_list:
-        beautiful_type = 'report'
-    elif GraphEntity.iri_series in ugly_types_list:
-        beautiful_type = 'series'
-    elif GraphEntity.iri_specification_document in ugly_types_list:
-        beautiful_type = 'standard'
-    elif GraphEntity.iri_web_content in ugly_types_list:
-        beautiful_type = 'web content'
-    return beautiful_type
+def __postprocess_type(types_uris:str) -> str:
+    types_uris_list = types_uris.split(' ;and; ')
+    types_uris_list.remove('http://purl.org/spar/fabio/Expression')
+    type_uri = types_uris_list[0]
+    type_string = URI_TYPE_DICT[type_uri]
+    return type_string
+
+class TextSearch():
+    def __init__(self, text:str):
+        self.text = text
+    
+    def get_text_search_on_title(self) -> str:
+        return f'''
+            ?res dcterm:title ?tsTitle.
+            {self.__gen_text_search('tsTitle', self.text, True)}
+        '''
+    
+    def get_text_search_on_person(self, role:str) -> str:
+        role = role.title()
+        return f'''
+            ?res pro:isDocumentContextFor ?ts{role}.
+            ?ts{role} pro:withRole pro:{role.lower()};
+                    pro:isHeldBy ?ts{role}Ra.
+            {{
+                ?ts{role}Ra foaf:familyName ?ts{role}Fn
+                {self.__gen_text_search(f'ts{role}Fn', self.text, False)}
+            }}
+            UNION
+            {{
+                ?ts{role}Ra foaf:name ?ts{role}Name
+                {self.__gen_text_search(f'ts{role}Name', self.text, False)}
+            }}
+            UNION
+            {{
+                ?ts{role}Ra foaf:givenName ?ts{role}Gn
+                {self.__gen_text_search(f'ts{role}Gn', self.text, False)}
+            }}
+        '''
+    
+    def get_text_search_on_publisher(self) -> str:
+        return f'''
+            ?res pro:isDocumentContextFor ?tsPublisher.
+            ?tsPublisher pro:withRole pro:publisher;
+                    pro:isHeldBy ?tsPublisherRa.
+            ?tsPublisherRa foaf:name ?tsPublisherName.
+            {self.__gen_text_search(f'tsPublisherName', self.text, True)}
+        '''
+    
+    def get_text_search_on_page(self) -> str:
+        pages_list = re.split('[^A-Za-z\d]+(?=[A-Za-z\d]+)', self.text)
+        starting_page = pages_list[0]
+        ending_page = pages_list[1] if len(pages_list) == 2 else None
+        text_search = f'''
+            ?res frbr:embodiment ?tsEmbodiment.
+            ?tsEmbodiment prism:startingPage ?tsStartingPage;
+                        prism:endingPage ?tsEndingPage.
+            {self.__gen_text_search(f'tsStartingPage', starting_page, True)}
+        '''
+        if ending_page:
+            text_search += self.__gen_text_search(f'tsEndingPage', ending_page, True)
+        return text_search
+    
+    def get_text_search_on_vi(self, vi:str) -> str:
+        v_or_i = vi.title()
+        return f'''
+            ?res frbr:partOf+ ?ts{v_or_i}.
+            ?ts{v_or_i} a fabio:Journal{v_or_i};
+                    fabio:hasSequenceIdentifier ?ts{v_or_i}Number.
+            {self.__gen_text_search(f'ts{v_or_i}Number', self.text, True)}
+        '''
+    
+    def get_text_search_on_venue(self) -> str:
+        return f'''
+            ?res frbr:partOf+ ?tsVenue.
+            ?tsVenue a fabio:Journal;
+                    dcterm:title ?tsVenueTitle.
+            {self.__gen_text_search('tsVenueTitle', self.text, True)}
+        '''
+
+    def __gen_text_search(self, variable:str, text:str, match_all_terms:bool) -> str:
+        header = 'SERVICE <http://www.bigdata.com/rdf/search#search> {\n'
+        body = f"\t?{variable} bds:search '{text}'"
+        body = body + "; bds:matchAllTerms 'true'." if match_all_terms else body
+        footer = '}'
+        text_search = header + body + footer
+        return text_search
+
+
+def generate_text_search(fields:str, text:str) -> str:
+    text_searches = []
+    text_search = TextSearch(text)
+    fields_list = \
+        ['title', 'author', 'editor', 'publisher', 'page', 'issue', 'volume', 'venue'] if fields == 'all' \
+        else fields.split('__') if '__' in fields \
+        else [fields]
+    for field in fields_list:
+        if field in {'editor', 'author'}:
+            text_searches.append(getattr(text_search, f'get_text_search_on_person')(field))
+        elif field in {'volume', 'issue'}:
+            text_searches.append(getattr(text_search, f'get_text_search_on_vi')(field))
+        else:
+            text_searches.append(getattr(text_search, f'get_text_search_on_{field}')())
+    return '{' + '} UNION \n {'.join(text_searches) + '}',
--- test/api_test.py
+++ test/api_test.py
@@ -13,7 +13,8 @@ api_manager = APIManager([CONFIG])
 api_base = 'http://127.0.0.1:8080/api/v1'
 
 class test_API(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         reset_server()
         meta_process = MetaProcess(config=os.path.join('test', 'api_test', 'meta_config.yaml'))
         run_meta_process(meta_process)
@@ -113,5 +114,289 @@ class test_API(unittest.TestCase):
         result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
         expected_output = status_expected, result_expected, format_expected
         self.assertEqual(output, expected_output)
+
+    def test_text_search_title(self):
+        operation_url = 'search'
+        request = 'title/Adaptive'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1007/978-1-4020-9632-7 isbn:9789048127108 isbn:9781402096327 meta:br/0601",
+                "title": "Adaptive Environmental Management",
+                "author": "",
+                "date": "2009",
+                "page": "",
+                "issue": "",
+                "volume": "",
+                "venue": "",
+                "type": "book",
+                "publisher": "Springer Science And Business Media Llc [crossref:297]",
+                "editor": "Allan, Catherine [orcid:0000-0003-2098-4759]; Stankey, George H."
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_author(self):
+        operation_url = 'search'
+        request = 'author/Carolynne Montijn'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1088/0022-3727/39/14/017 meta:br/0602",
+                "title": "Diffusion Correction To The Raether–Meek Criterion For The Avalanche-To-Streamer Transition",
+                "author": "Montijn, Carolynne; Ebert, Ute [orcid:0000-0003-3891-6869]",
+                "date": "2006-06-30",
+                "page": "2979-2992",
+                "issue": "14",
+                "volume": "39",
+                "venue": "Journal Of Physics D: Applied Physics [issn:0022-3727 issn:1361-6463]",
+                "type": "journal article",
+                "publisher": "Iop Publishing [crossref:266]",
+                "editor": ""
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_editor(self):
+        operation_url = 'search'
+        request = 'editor/Stankey, George H.'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1007/978-1-4020-9632-7 isbn:9789048127108 isbn:9781402096327 meta:br/0601",
+                "title": "Adaptive Environmental Management",
+                "author": "",
+                "date": "2009",
+                "page": "",
+                "issue": "",
+                "volume": "",
+                "venue": "",
+                "type": "book",
+                "publisher": "Springer Science And Business Media Llc [crossref:297]",
+                "editor": "Allan, Catherine [orcid:0000-0003-2098-4759]; Stankey, George H."
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_publisher(self):
+        operation_url = 'search'
+        request = 'publisher/Springer'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1007/978-1-4020-9632-7 isbn:9789048127108 isbn:9781402096327 meta:br/0601",
+                "title": "Adaptive Environmental Management",
+                "author": "",
+                "date": "2009",
+                "page": "",
+                "issue": "",
+                "volume": "",
+                "venue": "",
+                "type": "book",
+                "publisher": "Springer Science And Business Media Llc [crossref:297]",
+                "editor": "Allan, Catherine [orcid:0000-0003-2098-4759]; Stankey, George H."
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_page(self):
+        operation_url = 'search'
+        request = 'page/2979'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1088/0022-3727/39/14/017 meta:br/0602",
+                "title": "Diffusion Correction To The Raether–Meek Criterion For The Avalanche-To-Streamer Transition",
+                "author": "Montijn, Carolynne; Ebert, Ute [orcid:0000-0003-3891-6869]",
+                "date": "2006-06-30",
+                "page": "2979-2992",
+                "issue": "14",
+                "volume": "39",
+                "venue": "Journal Of Physics D: Applied Physics [issn:0022-3727 issn:1361-6463]",
+                "type": "journal article",
+                "publisher": "Iop Publishing [crossref:266]",
+                "editor": ""
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_page_hyphen(self):
+        operation_url = 'search'
+        request = 'page/2979-2992'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1088/0022-3727/39/14/017 meta:br/0602",
+                "title": "Diffusion Correction To The Raether–Meek Criterion For The Avalanche-To-Streamer Transition",
+                "author": "Montijn, Carolynne; Ebert, Ute [orcid:0000-0003-3891-6869]",
+                "date": "2006-06-30",
+                "page": "2979-2992",
+                "issue": "14",
+                "volume": "39",
+                "venue": "Journal Of Physics D: Applied Physics [issn:0022-3727 issn:1361-6463]",
+                "type": "journal article",
+                "publisher": "Iop Publishing [crossref:266]",
+                "editor": ""
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_issue(self):
+        operation_url = 'search'
+        request = 'issue/14'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1088/0022-3727/39/14/017 meta:br/0602",
+                "title": "Diffusion Correction To The Raether–Meek Criterion For The Avalanche-To-Streamer Transition",
+                "author": "Montijn, Carolynne; Ebert, Ute [orcid:0000-0003-3891-6869]",
+                "date": "2006-06-30",
+                "page": "2979-2992",
+                "issue": "14",
+                "volume": "39",
+                "venue": "Journal Of Physics D: Applied Physics [issn:0022-3727 issn:1361-6463]",
+                "type": "journal article",
+                "publisher": "Iop Publishing [crossref:266]",
+                "editor": ""
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_volume(self):
+        operation_url = 'search'
+        request = 'volume/39'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1088/0022-3727/39/14/017 meta:br/0602",
+                "title": "Diffusion Correction To The Raether–Meek Criterion For The Avalanche-To-Streamer Transition",
+                "author": "Montijn, Carolynne; Ebert, Ute [orcid:0000-0003-3891-6869]",
+                "date": "2006-06-30",
+                "page": "2979-2992",
+                "issue": "14",
+                "volume": "39",
+                "venue": "Journal Of Physics D: Applied Physics [issn:0022-3727 issn:1361-6463]",
+                "type": "journal article",
+                "publisher": "Iop Publishing [crossref:266]",
+                "editor": ""
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_venue(self):
+        operation_url = 'search'
+        request = 'venue/Physics'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1088/0022-3727/39/14/017 meta:br/0602",
+                "title": "Diffusion Correction To The Raether–Meek Criterion For The Avalanche-To-Streamer Transition",
+                "author": "Montijn, Carolynne; Ebert, Ute [orcid:0000-0003-3891-6869]",
+                "date": "2006-06-30",
+                "page": "2979-2992",
+                "issue": "14",
+                "volume": "39",
+                "venue": "Journal Of Physics D: Applied Physics [issn:0022-3727 issn:1361-6463]",
+                "type": "journal article",
+                "publisher": "Iop Publishing [crossref:266]",
+                "editor": ""
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+    def test_text_search_all(self):
+        operation_url = 'search'
+        request = 'all/Stankey, George H.'
+        call = "%s/%s/%s" % (api_base, operation_url, request)
+        op = api_manager.get_op(call)
+        status, result, format = op.exec()
+        status_expected = 200
+        format_expected = 'application/json'
+        result_expected = [
+            {
+                "id": "doi:10.1007/978-1-4020-9632-7 isbn:9789048127108 isbn:9781402096327 meta:br/0601",
+                "title": "Adaptive Environmental Management",
+                "author": "",
+                "date": "2009",
+                "page": "",
+                "issue": "",
+                "volume": "",
+                "venue": "",
+                "type": "book",
+                "publisher": "Springer Science And Business Media Llc [crossref:297]",
+                "editor": "Allan, Catherine [orcid:0000-0003-2098-4759]; Stankey, George H."
+            }
+        ]
+        output = status, [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in json.loads(result)], format
+        result_expected = [{k:set(v.split('; ')) if k in {'author', 'editor'} else set(v.split()) if k == 'id' else v for k,v in el.items()} for el in result_expected]
+        expected_output = status_expected, result_expected, format_expected
+        self.assertEqual(output, expected_output)
+
+
+
+
 
 
