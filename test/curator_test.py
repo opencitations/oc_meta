6a5f131748e9ffe230f8f5d283bdde01f8eea687
arcangelo7 <arcangelomas@gmail.com>
2023-05-01 01:42:10 +0200

    curator test fix

--- test/curator_test.py
+++ test/curator_test.py
@@ -58,7 +58,7 @@ def add_data_ts(server:str=SERVER, data_path:str=os.path.abspath(os.path.join('t
 def store_curated_data(curator_obj:Curator, server:str) -> None:
     creator_obj = Creator(curator_obj.data, SERVER, BASE_IRI, None, None, 'https://orcid.org/0000-0002-8420-0696',
                             curator_obj.index_id_ra, curator_obj.index_id_br, curator_obj.re_index,
-                            curator_obj.ar_index, curator_obj.VolIss, preexisting_entities=set())
+                            curator_obj.ar_index, curator_obj.VolIss, preexisting_entities=set(), everything_everywhere_allatonce=Graph())
     creator = creator_obj.creator(source=None)
     res_storer = Storer(creator)
     res_storer.upload_all(server, base_dir=None, batch_size=100)
