d490ec52cc66c53c49e7af46d21991b30e46accb
arcangelo7 <arcangelomas@gmail.com>
2023-11-27 15:43:35 +0100

    export kb tqdm in multiprocessing

--- oc_meta/run/export_kb.py
+++ oc_meta/run/export_kb.py
@@ -1,12 +1,13 @@
 import argparse
 import multiprocessing
 import os
-
+import time
 from rdflib import ConjunctiveGraph, Literal, URIRef
 from SPARQLWrapper import JSON, SPARQLWrapper
+from tqdm import tqdm
+import queue
 
-
-def query_triplestore(endpoint, offset, limit):
+def query_triplestore(endpoint, offset, limit, max_retries=5):
     sparql = SPARQLWrapper(endpoint)
     sparql.setQuery(f"""
         SELECT ?g ?s ?p ?o WHERE {{
@@ -14,8 +15,13 @@ def query_triplestore(endpoint, offset, limit):
         }} LIMIT {limit} OFFSET {offset}
     """)
     sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    return results
+    for attempt in range(max_retries):
+        try:
+            results = sparql.query().convert()
+            return results
+        except Exception as e:
+            print(f"Errore nella query: {e}, tentativo {attempt + 1} di {max_retries}")
+            time.sleep(2 ** attempt)  # Backoff esponenziale
 
 def count_triples(endpoint):
     sparql = SPARQLWrapper(endpoint)
@@ -51,7 +57,7 @@ def convert_to_graph(results):
         g.add((s, p, o, graph_uri))
     return g
 
-def process_task(endpoint, output_folder, page_size, offset, file_count):
+def process_task(endpoint, output_folder, page_size, offset, file_count, progress_queue):
     results = query_triplestore(endpoint, offset, page_size)
     if results["results"]["bindings"]:
         graph = convert_to_graph(results)
@@ -59,33 +65,56 @@ def process_task(endpoint, output_folder, page_size, offset, file_count):
         with open(output_filename, 'w', encoding='utf-8') as f:
             jsonld_data = graph.serialize(format='json-ld')
             f.write(jsonld_data)
+    progress_queue.put(1)
+
+def progress_monitor(total_tasks, progress_queue):
+    with tqdm(total=total_tasks, desc="Downloading and Saving Triples") as pbar:
+        completed_tasks = 0
+        while completed_tasks < total_tasks:
+            completed_tasks += progress_queue.get()
+            pbar.update(1)
+
+def process_worker(endpoint, output_folder, page_size, task_queue, progress_queue):
+    while not task_queue.empty():
+        try:
+            offset, file_count = task_queue.get_nowait()
+            process_task(endpoint, output_folder, page_size, offset, file_count, progress_queue)
+        except queue.Empty:
+            break
 
 def main(endpoint, output_folder, page_size, n_processes):
     os.makedirs(output_folder, exist_ok=True)
 
     total_triples = count_triples(endpoint)
     num_pages = (total_triples + page_size - 1) // page_size
-    pages_per_process = (num_pages + n_processes - 1) // n_processes
 
-    processes = []
-    for i in range(n_processes):
-        start_offset = i * pages_per_process * page_size
-        for j in range(pages_per_process):
-            offset = start_offset + j * page_size
-            file_count = i * pages_per_process + j
-            if file_count < num_pages:
-                proc = multiprocessing.Process(target=process_task, args=(endpoint, output_folder, page_size, offset, file_count))
-                processes.append(proc)
-                proc.start()
+    manager = multiprocessing.Manager()
+    progress_queue = manager.Queue()
+    task_queue = manager.Queue()
+
+    for file_count in range(num_pages):
+        offset = file_count * page_size
+        task_queue.put((offset, file_count))
+
+    monitor = multiprocessing.Process(target=progress_monitor, args=(num_pages, progress_queue))
+    monitor.start()
+
+    processes = [multiprocessing.Process(target=process_worker, args=(endpoint, output_folder, page_size, task_queue, progress_queue)) for _ in range(n_processes)]
+
+    for proc in processes:
+        proc.start()
 
     for proc in processes:
         proc.join()
+
+    monitor.join()  # Assicurati che il monitor del progresso termini correttamente
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download triples from a Blazegraph triplestore and save as JSON-LD.")
     parser.add_argument("--endpoint", required=True, help="SPARQL endpoint URL")
     parser.add_argument("--output", required=True, help="Folder path to save output JSON-LD files")
-    parser.add_argument("--pagesize", type=int, default=1000, help="Number of triples per page (default: 1000)")
+    parser.add_argument("--pagesize", type=int, default=10000, help="Number of triples per page (default: 1000)")
     parser.add_argument("--nprocesses", type=int, default=4, help="Number of processes to use (default: 4)")
 
     args = parser.parse_args()
